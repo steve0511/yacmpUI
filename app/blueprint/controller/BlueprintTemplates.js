2fa6744f5a1eec9ddbcfc468f47d8e50bd994097
Hanyu Zhang <hanyuzhang@vmware.com>
2016-01-22 10:10:32 +0800

    Add more template snippets for Openstack In this change, we have add more template snippets for Openstack. It includes: 1. Template for floating IP. Associate it with server via cloudify.openstack.server_connected_to_floating_ip relationship 2. Template for security group. Associate it with server via cloudify.openstack.server_connected_to_security_group relationship 3. Template for Keypair. Associate it with cloudify.openstack.server_connected_to_keypair relationship relationship 4. Template for Volume. Associate it with cloudify.openstack.volume_attached_to_server relationship
    When creating a blueprint, we should first create these template nodes. To associate them with a server node, use the relationships above to associate them with server nodes, in server's relationships field.

--- app/blueprint/controller/BlueprintTemplates.js
+++ app/blueprint/controller/BlueprintTemplates.js
@@ -9,14 +9,55 @@
 // can insert their own template
 angular.module('yacmpApp').service('BlueprintTemplates', [
     function () {
-        this.blueprintTemplate = 'tosca_definitions_version: tosca_simple_yaml_1_0\n\ndescription: #Your description here\n\ntopology_template:\n  inputs:\n    #Input parameters\n\n  node_templates:\n    #Your node definitions here';
+        this.blueprintTemplate = 'tosca_definitions_version: cloudify_dsl_1_2\n\ndescription: # Your description here\n\ninputs:\n  # Input parameters\n\nnode_templates:\n  # Your node definitions here';
 
+        // TODO: For now, we will only support Openstack. In future, we will have different template providers and the final UI will show different
+        // types of template snippets. Then the this.templates variable will store objects like below:
+        // {
+        //     type: 'Openstack',
+        //     snippets: [{
+        //         name: 'Compute Node',
+        //         content: ...
+        //     }]
+        // }
+        // And these templates should be retrieved from server. For now, we will hard code them here
         this.templates = [];
 
-        var computeNodeTemplate = {};
-        computeNodeTemplate.id = '1';
-        computeNodeTemplate.name = 'Compute Node';
-        computeNodeTemplate.content = '\n    type: tosca.nodes.Compute\n      capabilities:\n        # Host container properties\n          host:\n          properties:\n            # Compute properties\n            num_cpus: 4\n            mem_size: 2048 MB\n            disk_size: 10 GB\n';
-        this.templates.push(computeNodeTemplate);
+        var serverTemplate = {};
+        serverTemplate.name = 'Server';
+        serverTemplate.description = 'Creates an Openstack server instance';
+        serverTemplate.content = '\n  # Server name\n    type: cloudify.openstack.nodes.Server\n      server: # Server configurations\n    relationships: # Relationships\n';
+        this.templates.push(serverTemplate);
+
+        var floatingIPTemplate = {};
+        floatingIPTemplate.name = 'Floating IP';
+        floatingIPTemplate.description = 'Creates a floating IP to be attached to a specific host. Use server\'s relationships field to associate it with this floating IP, via cloudify.openstack.server_connected_to_floating_ip relationship';
+        floatingIPTemplate.content = '\n  # Floating IP name\n    type: cloudify.openstack.nodes.FloatingIP\n';
+        this.templates.push(floatingIPTemplate);
+
+        var securityGroupTemplate = {};
+        securityGroupTemplate.name = 'Security Group';
+        securityGroupTemplate.description = 'Create a security group to be assigned to a specific host, to avoid invalid access. Use server\'s relationships field to associate it with this security group, via cloudify.openstack.server_connected_to_security_group relationship';
+        securityGroupTemplate.content = '\n  # Security group name\n  type: cloudify.openstack.nodes.SecurityGroup\n    properties:\n      rules:\n        - remote_ip_prefix: # IP prefix\n          port: # Port number\n';
+        this.templates.push(securityGroupTemplate);
+
+        var keyPairTemplate = {};
+        keyPairTemplate.name = 'KeyPair';
+        keyPairTemplate.description = 'Create a keypair which could be connected with a server node via cloudify.openstack.server_connected_to_keypair relationship';
+        keyPairTemplate.content = '\n  # Key pair name\n  type: cloudify.openstack.nodes.KeyPair\n    properties:\n      public_key:\n        user: # User name\n        key: # Public key\n';
+        this.templates.push(keyPairTemplate);
+
+        var volumeTemplate = {};
+        volumeTemplate.name = 'Volume';
+        volumeTemplate.description = 'Create a volume which could be attached to a server node via cloudify.openstack.volume_attached_to_server';
+        volumeTemplate.content = '\n  # Volume name\n  type: cloudify.openstack.nodes.Volume\n    Properties:\n      size: # Size';
+        this.templates.push(volumeTemplate);
+        // cloudify.openstack.nodes.Subnet
+        // cloudify.openstack.nodes.Router
+        // cloudify.openstack.nodes.Port
+        // cloudify.openstack.nodes.Network
+        // cloudify.openstack.nodes.Volume
+        // cloudify.openstack.nova_net.nodes.FloatingIP
+        // cloudify.openstack.nova_net.nodes.SecurityGroup
     }
 ]);
