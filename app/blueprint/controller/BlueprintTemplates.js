1fe2d39913dca31d2e4e7e92d873b0a9c2f46619
Hanyu Zhang <hanyuzhang@vmware.com>
2016-01-26 15:24:09 +0800

    Add templates for vSphere

--- app/blueprint/controller/BlueprintTemplates.js
+++ app/blueprint/controller/BlueprintTemplates.js
@@ -21,43 +21,46 @@ angular.module('yacmpApp').service('BlueprintTemplates', [
         //     }]
         // }
         // And these templates should be retrieved from server. For now, we will hard code them here
-        this.templates = [];
+        this.templates = {
+            openstack: [{
+                name: 'Server',
+                description: 'Creates an Openstack server instance',
+                content: '\n  # Server name\n    type: cloudify.openstack.nodes.Server\n      server: # Server configurations\n    relationships: # Relationships\n'
+            }, {
+                name: 'Floating IP',
+                description: 'Creates a floating IP to be attached to a specific host. Use server\'s relationships field to associate it with this floating IP, via cloudify.openstack.server_connected_to_floating_ip relationship',
+                content: '\n  # Floating IP name\n    type: cloudify.openstack.nodes.FloatingIP\n'
+            }, {
+                name: 'Security Group',
+                description: 'Create a security group to be assigned to a specific host, to avoid invalid access. Use server\'s relationships field to associate it with this security group, via cloudify.openstack.server_connected_to_security_group relationship',
+                content: '\n  # Security group name\n  type: cloudify.openstack.nodes.SecurityGroup\n    properties:\n      rules:\n        - remote_ip_prefix: # IP prefix\n          port: # Port number\n'
+            }, {
+                name: 'KeyPair',
+                description: 'Create a keypair which could be connected with a server node via cloudify.openstack.server_connected_to_keypair relationship',
+                content: '\n  # Key pair name\n  type: cloudify.openstack.nodes.KeyPair\n    properties:\n      public_key:\n        user: # User name\n        key: # Public key\n'
+            }, {
+                name: 'Volume',
+                description: 'Create a volume which could be attached to a server node via cloudify.openstack.volume_attached_to_server',
+                content: '\n  # Volume name\n  type: cloudify.openstack.nodes.Volume\n    Properties:\n      size: # Size'
+            }],
+            // cloudify.openstack.nodes.Subnet
+            // cloudify.openstack.nodes.Router
+            // cloudify.openstack.nodes.Port
+            // cloudify.openstack.nodes.Network
+            // cloudify.openstack.nodes.Volume
+            // cloudify.openstack.nova_net.nodes.FloatingIP
+            // cloudify.openstack.nova_net.nodes.SecurityGroup
 
-        var serverTemplate = {};
-        serverTemplate.name = 'Server';
-        serverTemplate.description = 'Creates an Openstack server instance';
-        serverTemplate.content = '\n  # Server name\n    type: cloudify.openstack.nodes.Server\n      server: # Server configurations\n    relationships: # Relationships\n';
-        this.templates.push(serverTemplate);
-
-        var floatingIPTemplate = {};
-        floatingIPTemplate.name = 'Floating IP';
-        floatingIPTemplate.description = 'Creates a floating IP to be attached to a specific host. Use server\'s relationships field to associate it with this floating IP, via cloudify.openstack.server_connected_to_floating_ip relationship';
-        floatingIPTemplate.content = '\n  # Floating IP name\n    type: cloudify.openstack.nodes.FloatingIP\n';
-        this.templates.push(floatingIPTemplate);
-
-        var securityGroupTemplate = {};
-        securityGroupTemplate.name = 'Security Group';
-        securityGroupTemplate.description = 'Create a security group to be assigned to a specific host, to avoid invalid access. Use server\'s relationships field to associate it with this security group, via cloudify.openstack.server_connected_to_security_group relationship';
-        securityGroupTemplate.content = '\n  # Security group name\n  type: cloudify.openstack.nodes.SecurityGroup\n    properties:\n      rules:\n        - remote_ip_prefix: # IP prefix\n          port: # Port number\n';
-        this.templates.push(securityGroupTemplate);
-
-        var keyPairTemplate = {};
-        keyPairTemplate.name = 'KeyPair';
-        keyPairTemplate.description = 'Create a keypair which could be connected with a server node via cloudify.openstack.server_connected_to_keypair relationship';
-        keyPairTemplate.content = '\n  # Key pair name\n  type: cloudify.openstack.nodes.KeyPair\n    properties:\n      public_key:\n        user: # User name\n        key: # Public key\n';
-        this.templates.push(keyPairTemplate);
-
-        var volumeTemplate = {};
-        volumeTemplate.name = 'Volume';
-        volumeTemplate.description = 'Create a volume which could be attached to a server node via cloudify.openstack.volume_attached_to_server';
-        volumeTemplate.content = '\n  # Volume name\n  type: cloudify.openstack.nodes.Volume\n    Properties:\n      size: # Size';
-        this.templates.push(volumeTemplate);
-        // cloudify.openstack.nodes.Subnet
-        // cloudify.openstack.nodes.Router
-        // cloudify.openstack.nodes.Port
-        // cloudify.openstack.nodes.Network
-        // cloudify.openstack.nodes.Volume
-        // cloudify.openstack.nova_net.nodes.FloatingIP
-        // cloudify.openstack.nova_net.nodes.SecurityGroup
+            vSphere: [{
+                name: 'Server',
+                description: 'Creates an vSphere server instance',
+                content: '\n  # Server name\n    type: cloudify.vsphere.nodes.server\n      server: # Server configurations\n    relationships: # Relationships\n'
+            }, {
+                name: 'cloudify.vsphere.nodes.storage',
+                description: 'Create a storage which could be attached to a vSphere server node via cloudify.vsphere.volume_attached_to_server',
+                content: '\n  # Volume name\n  type: cloudify.openstack.nodes.Volume\n    Properties:\n      size: # Size'
+            }]
+        }
+        // cloudify.vsphere.nodes.network
     }
 ]);
